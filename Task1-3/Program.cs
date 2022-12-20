85fffc449613137be9d0164e68e46e4fb140dc05
serg-zakhar <120890742+serg-zakhar@users.noreply.github.com>
2022-12-20 17:08:22 +0300

    Delete Task1-3 directory

--- Task1-3/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿Console.WriteLine("Введите число:");
-int number = Convert.ToInt32(Console.ReadLine());
-if (number % 2 == 0) Console.WriteLine($"Число {number} четное");
-else Console.WriteLine($"Число {number} нечетное");
