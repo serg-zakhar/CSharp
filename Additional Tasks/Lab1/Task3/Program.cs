0ceee603e5f542502b5988968cbcfce3c81e79c2
serg-zakhar <szakhar@gmail.com>
2022-12-20 17:52:13 +0300

    Make Lab1\Task3

---
+++ Additional Tasks/Lab1/Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿Random rnd = new Random();
+double c = rnd.Next(-10, 10);
+double d = rnd.Next(-10, 10);
+if (c % 2 == 0 && d % 2 == 0)
+{
+    Console.WriteLine($"Частное чисел {c} и {d} равно: {c / d}");
+}
+else if (c % 2 != 0 && d % 2 != 0)
+{
+    Console.WriteLine($"Сумма чисел {c} и {d} равна: {c + d}");
+}
+else
+{
+    Console.WriteLine($"Произведение чисел {c} и {d} равно: {c * d}");
+}
