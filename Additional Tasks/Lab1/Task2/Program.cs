561113e0e82745931b53d5c0034d95aaf2ca70d0
serg-zakhar <szakhar@gmail.com>
2022-12-20 17:40:27 +0300

    Make Lab1\Task2

---
+++ Additional Tasks/Lab1/Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿Random rnd = new Random();
+int a = rnd.Next(10, 30);
+int b = rnd.Next(10, 30);
+if (a > b)
+{
+    Console.WriteLine($"Разность чисел {a} и {b} равна: {a - b}");
+}
+else if (a == b)
+{
+    Console.WriteLine("Числа равны");
+}
+else
+{
+    Console.WriteLine($"Сумма чисел {a} и {b} равна: {a + b}");
+}
