82c88ac3a37c218ac4677c332d57438524c78110
serg-zakhar <szakhar@gmail.com>
2022-12-24 08:32:37 +0300

    make task19

--- Task3/Task19/Program.cs
+++ Task3/Task19/Program.cs
@@ -2,24 +2,29 @@
 // и проверяет, является ли оно палиндромом.
 
 Console.WriteLine("Введите пятизначное число: ");
-string strNumber = Console.ReadLine();
-int[] n = new int[strNumber.Length];
+string? strNumber = Console.ReadLine();
+if (strNumber is not null)
+{
+    int[] n = new int[strNumber.Length];
 
-if (strNumber.Length != 5)
-{
-    Console.WriteLine("Число не пятизначное!");
-}
-else
-{
-    for (int i = 0; i < strNumber.Length; i++)
+    if (strNumber.Length != 5)
     {
-        n[i] = int.Parse(strNumber[i].ToString());
+        Console.WriteLine("Число не пятизначное!");
     }
-    if (n[0] == n[4] && n[1] == n[3])
+    else
     {
-        Console.WriteLine($"Число {strNumber} является палиндромом");
+        for (int i = 0; i < strNumber?.Length; i++)
+        {
+            n[i] = int.Parse(strNumber[i].ToString());
+        }
+        if (n[0] == n[4] && n[1] == n[3])
+        {
+            Console.WriteLine($"Число {strNumber} является палиндромом");
 
-    } else {
-        Console.WriteLine($"Число {strNumber} не является палиндромом");
+        }
+        else
+        {
+            Console.WriteLine($"Число {strNumber} не является палиндромом");
+        }
     }
 }
