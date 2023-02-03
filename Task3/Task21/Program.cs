0532b128fb289b879b1cc35e10da591a38f07a36
serg-zakhar <szakhar@gmail.com>
2023-02-03 06:48:13 +0300

    Modify T21 T23

--- Task3/Task21/Program.cs
+++ Task3/Task21/Program.cs
@@ -3,17 +3,17 @@
 
 Console.Clear();
 Console.Write("Введите X1: ");
-int x1 = int.Parse(Console.ReadLine());
+int x1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите Y1: ");
-int y1 = int.Parse(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите Z1: ");
-int z1 = int.Parse(Console.ReadLine());
+int z1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите X2: ");
-int x2 = int.Parse(Console.ReadLine());
+int x2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите Y2: ");
-int y2 = int.Parse(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите Z2: ");
-int z2 = int.Parse(Console.ReadLine());
+int z2 = Convert.ToInt32(Console.ReadLine());
 
 double d = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));
 
