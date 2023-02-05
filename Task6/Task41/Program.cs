7f6afe86fc982b8653ee367471822935eb20d4e1
serg-zakhar <szakhar@gmail.com>
2023-02-05 09:41:40 +0300

    Make T41 T43

---
+++ Task6/Task41/Program.cs
@@ -0,0 +1,17 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь
+
+Console.Clear();
+Console.WriteLine("Задайте количество вводимых чисел: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+int countPos = 0;
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine($"Введите {i+1} число: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    if (array[i] > 0) countPos += 1;
+}
+
+Console.WriteLine($"[{String.Join(", ", array)}]");
+Console.WriteLine($"Вы ввели {countPos} положительных чисел");
---
+++ Task6/Task43/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.Clear();
+Console.WriteLine("Программа выводит координаты точки пересечения двух прямых,");
+Console.WriteLine("заданных уравнением y = kx + b");
+Console.WriteLine("Введите коэфициенты для первого уравнения");
+Console.WriteLine("k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите коэфициенты для второго уравнения");
+Console.WriteLine("k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.WriteLine($"Координаты точки: {{{x}, {y}}} ");
