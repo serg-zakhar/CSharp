97674d80f2b073692f1ec430af9633da30d11ccf
serg-zakhar <szakhar@gmail.com>
2023-02-06 14:07:12 +0300

    correct t52

--- Task7/Task52/Program.cs
+++ Task7/Task52/Program.cs
@@ -2,46 +2,45 @@
 // Найдите среднее арифметическое элементов в каждом столбце.
 
 Console.Clear();
-Console.Clear();
 Console.WriteLine("Задайте размерность и диапазон значений массива");
 Console.WriteLine("Количество строк: ");
-int m = Convert.ToInt32(Console.ReadLine());
+int row = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Количество столбцов: ");
-int n = Convert.ToInt32(Console.ReadLine());
+int col = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Минимальное значение: ");
 int min = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Максимальное значение: ");
 int max = Convert.ToInt32(Console.ReadLine());
 
-int[,] array = GetArray(m, n, min, max);
+int[,] array = GetArray(row, col, min, max);
 
 PrintArray(array);
-for (int i = 0; i < m; i++)
+for (int i = 0; i < col; i++)
 {
     Console.WriteLine($"Среднее арифметическое {i+1} столбца равно: {GetMidVal(array, i)}");
 }
 
 
-double GetMidVal(int[,] array, int n)
+double GetMidVal(int[,] array, int col)
 {
     double sum = 0;
     double result = 0;
     //Console.WriteLine(array.GetLength(0));
     for (int i = 0; i < array.GetLength(0); i++)
     {
-        sum += array[i, n];
-        //Console.WriteLine(array[i,n]);
+        sum += array[i, col];
+        //Console.WriteLine(array[i,col]);
     }
     result = sum / array.GetLength(0);
     return Math.Round(result, 2);
 }
 
-int[,] GetArray(int m, int n, int minValue, int maxValue)
+int[,] GetArray(int row, int col, int minValue, int maxValue)
 {
-    int[,] result = new int[m, n];
-    for (int i = 0; i < m; i++)
+    int[,] result = new int[row, col];
+    for (int i = 0; i < row; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < col; j++)
         {
             result[i, j] = new Random().Next(minValue, maxValue + 1);
         }
